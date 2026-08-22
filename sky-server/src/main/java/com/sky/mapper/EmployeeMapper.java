d738c803b0e5844e42d01b333c3dac740f0a772d
fcz20060311 <fcz20060311@gmail.com>
2026-08-22 16:24:29 +0800

    fix:修复了传递数据库时传递错误属性

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -15,6 +15,7 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
-    @Select("insert into employee (username, password, name, status, create_time, update_time, create_user, update_user,phone,sex) values (#{username}, #{password}, #{name}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser}, #{phone}, #{sex})")
+//    后面进入的值需要根据EmployeeDTO对象的属性来进行传入
+    @Select("insert into employee (id,name,username,password,phone,sex,id_number,status,create_time,update_time,create_user,update_user  ) values (#{id},#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
     void insert(Employee employee);
 }
