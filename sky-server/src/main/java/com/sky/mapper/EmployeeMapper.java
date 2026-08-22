b73f749593d6cff5bc2c5cac90e7a5dfa06d3876
fcz20060311 <fcz20060311@gmail.com>
2026-08-22 16:09:25 +0800

    fix:修复了传递数据时遗漏了性别这一栏

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -15,6 +15,6 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
-    @Select("insert into employee (username, password, name, status, create_time, update_time, create_user, update_user,phone) values (#{username}, #{password}, #{name}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser}, #{phone})")
+    @Select("insert into employee (username, password, name, status, create_time, update_time, create_user, update_user,phone,sex) values (#{username}, #{password}, #{name}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser}, #{phone}, #{sex})")
     void insert(Employee employee);
 }
