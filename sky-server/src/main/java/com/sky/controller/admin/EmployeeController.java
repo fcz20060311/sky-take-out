22bbeaeaecda8c4efbd0118c7ec933c4810e72c6
fcz20060311 <fcz20060311@gmail.com>
2026-08-23 08:17:07 +0800

    feat:新增功能分页查询

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.sky.controller.admin;
 
+import com.github.pagehelper.Page;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
@@ -14,12 +17,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -87,5 +88,14 @@ public class EmployeeController {
         return null;
 
 
+    }
+
+
+    @ApiOperation("分页查询")
+    @GetMapping("/page")
+    public Result<PageResult> pageQuery(@RequestBody EmployeePageQueryDTO employeePageQueryDTO){
+        log.info("分页查询:{}", employeePageQueryDTO);
+        PageResult pageresult=employeeService.pagequerry(employeePageQueryDTO);
+        return Result.success(pageresult);
     }
 }
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -18,4 +20,7 @@ public interface EmployeeMapper {
 //    后面进入的值需要根据EmployeeDTO对象的属性来进行传入
     @Select("insert into employee (id,name,username,password,phone,sex,id_number,status,create_time,update_time,create_user,update_user  ) values (#{id},#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
     void insert(Employee employee);
+
+//    这个的快捷键是alt+shift+enter
+    Page<Employee> pagequery(EmployeePageQueryDTO employeePageQueryDTO);
 }
